8a944b7f67c41002c76e3993601ccd471da91e9c
Bage <bagerathi.s@gmail.com>
2022-11-11 11:19:51 +0530

    Add project files.

---
+++ mvc/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminLogin()
+        {
+            return View();
+        }
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
